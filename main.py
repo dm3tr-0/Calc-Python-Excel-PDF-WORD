73bc4e112136d680b883b8295e6dffad225cc879
mitya <140421001+dm3tr-0@users.noreply.github.com>
2024-10-08 21:06:21 +0500

    добавил комментарии

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+#эта функция срабатывает, когда нажали кнопку рассчитать(index.html id=calculate)
 @app.route('/calculate', methods=['POST'])
 def calculate():
+    #получаем словарь со значениями
     data = request.json
+    #вытаскиваем значения по ключу и сохраняем в переменные
     work_hours = float(data['work_hours'])
     days_count = float(data['days_count'])
     max_files_day = float(data['max_files_day'])
 
-    # Пример расчетов
+    #пример расчетов
     max_files_month = max_files_day * days_count
     fact_files_month = work_hours * 0.9  # Условный расчет
     load_difference = max_files_month - fact_files_month
 
+    #сохраняем полученные расчеты в словарь result
     result = {
         'max_files_month': max_files_month,
         'fact_files_month': fact_files_month,
         'load_difference': load_difference
     }
-
+    #возвращаем полученные расчеты (они отобразяться в Итоговые результаты id=result-output)
     return jsonify(result)
 
 
