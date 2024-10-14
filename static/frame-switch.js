b0be802c36d58953b564e8f2eab3ecbb6f38c0f7
mitya <140421001+dm3tr-0@users.noreply.github.com>
2024-10-15 00:39:27 +0500

    рефракторинг frame-switch.js

--- static/frame-switch.js
+++ static/frame-switch.js
@@ -1,15 +1,19 @@
 //меняет фреймы в зависимости от выбранного режима
 const calculatorType = document.getElementById('calculator-type');
+const mode0 = document.getElementById('mode-0');
 const mode1 = document.getElementById('mode-1');
 const mode2 = document.getElementById('mode-2');
 const mode3 = document.getElementById('mode-3');
-
+mode0.style.display = 'block';
 calculatorType.addEventListener('change', function() {
+    mode0.style.display = 'none';
     mode1.style.display = 'none';
     mode2.style.display = 'none';
     mode3.style.display = 'none';
 
-    if (this.value === '1') {
+    if (this.value === '0') {
+        mode0.style.display = 'block';
+    }else if(this.value === '1') {
         mode1.style.display = 'block';
     } else if (this.value === '2') {
         mode2.style.display = 'block';
