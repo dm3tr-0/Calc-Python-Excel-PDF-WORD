eb63c2af098ee32f42a8be3284c5a7dc5e0b1418
mitya <140421001+dm3tr-0@users.noreply.github.com>
2024-11-24 23:47:19 +0500

    исправил зашитые значения

--- settings.py
+++ settings.py
@@ -2,6 +2,12 @@
 #зашитые значения:
 ###################
 
+#время обработки(мин)
+time_obr_day = 8 #день
+time_obr_night = 6 #ночь
+
+#кол-во минут в часе
+min_in_hour = 50
 #количество часов работы для 180ч, 168ч, 79ч, 180ч пр/вых, 180ч ночь соответсвтенно
 work_hours = {
     '180h' : 11,
@@ -20,17 +26,17 @@ mid_days_count = {
 }
 #максимальное колвичество файлов в день для 180ч, 168ч, 79ч, 180ч пр/вых, 180ч ночь соответсвтенно
 max_files_day = {
-    '180h': 58,
-    '168h': 43,
-    '79h': 21,
-    '180h_pr': 78,
-    '180h_night': 78
+    '180h': work_hours['180h'] * min_in_hour / time_obr_day * 0.85, #58.43
+    '168h': work_hours['168h'] * min_in_hour / time_obr_day * 0.85, #42.5
+    '79h': work_hours['79h'] * min_in_hour / time_obr_day * 0.85, #21.25
+    '180h_pr': work_hours['180h_pr'] * min_in_hour / time_obr_night * 0.85, #77.92
+    '180h_night': work_hours['180h_night'] * min_in_hour / time_obr_night * 0.85 #77.92
 }
 # максимальное колвичество файлов в месяц для 180ч, 168ч, 79ч, 180ч пр/вых, 180ч ночь соответсвтенно
 max_files_month = {
-    '180h': 877,
-    '168h': 850,
-    '79h': 425,
-    '180h_pr': 1169,
-    '180h_night': 1169
+    '180h': mid_days_count['180h'] * max_files_day['180h'], #876.56
+    '168h': mid_days_count['168h'] * max_files_day['168h'], #850.0
+    '79h': mid_days_count['79h'] * max_files_day['79h'], #425.0
+    '180h_pr': mid_days_count['180h_pr'] * max_files_day['180h_pr'], #1168.75
+    '180h_night': mid_days_count['180h_night'] * max_files_day['180h_night'] #1168.75
 }
